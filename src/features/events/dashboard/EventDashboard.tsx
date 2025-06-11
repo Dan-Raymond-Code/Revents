fa48c4fc26f8bf7b235c7687e4201cbab894234f
Dan Raymond <kip.raymond@gmail.com>
2025-06-10 20:05:32 -0600

    End of Section 21.

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react"; 
 import { events } from "../../../lib/data/sampleData";
 import EventForm from "../form/EventForm";
 import EventCard from "./EventCard";
+import type { AppEvent } from "../../../lib/types";
 
 type Props = {
   formOpen: boolean;
@@ -8,19 +10,28 @@ type Props = {
 }
 
 export default function EventDashboard({ formOpen, setFormOpen }: Props) {
-  // This is a placeholder for the actual event data fetching logic
+  const [appEvents, setAppEvents] = useState<AppEvent[]>([]);
+
+  useEffect(() => {
+     setAppEvents(events);  
+
+     return () => {
+       setAppEvents([]); // Cleanup function to reset events on unmount
+     }
+  }, []);  
+
   return (
     <div className="flex flex-row w-full gap-6">
       <div className="w-3/5 flex flex-col gap-4">
-      {events.map((event) => (
-        <EventCard key={event.id} event={event} />
-      ))
-      }
+        {appEvents.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))
+        }
       </div>
       <div className="w-2/5">
-      {formOpen && (
-        <EventForm setFormOpen={() => setFormOpen()} />
-      )}
+        {formOpen && (
+          <EventForm setFormOpen={() => setFormOpen(false)} />
+        )}
       </div>
     </div>
   )
