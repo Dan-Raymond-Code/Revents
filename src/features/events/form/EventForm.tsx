d85261cc982d746885792ebcd7601f0a890da488
Dan Raymond <kip.raymond@gmail.com>
2025-06-11 15:08:51 -0600

    Completed Sec 4.25.

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -3,20 +3,35 @@ type Props = {
 }
 
 export default function EventForm({setFormOpen}: Props) {
-  
-  return (
+  // const onSubmit = (formData: FormData) => {
+  //   console.log(formData);
+
+  //   const data = Object.fromEntries(formData.entries());
+  //   console.log(data);
+  // }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+    // Here you would typically send the data to your backend API    
+    setFormOpen(false); // Close the form after submission      
+  }
+
+  return (  
     <div className="card bg-base-100 p-4 flex flex-col gap-3 w-full">
         <h2 className="text-2xl font-semibold text-center text-primary">Create New Event</h2>
-        <form className="flex flex-col gap-3 w-full">
-            <input type="text" className="input input-lg w-full" placeholder="Event title" />
-            <input type="text" className="input input-lg w-full" placeholder="Category" />            
-            <textarea className="textarea textarea-lg w-full" placeholder="Description" />
-              <input type="text" className="input input-lg w-full" placeholder="Date" />
-              <input type="text" className="input input-lg w-full" placeholder="City" />
-              <input type="text" className="input input-lg w-full" placeholder="Venue" />
+        <form onSubmit={handleSubmit} method="post" className="flex flex-col gap-3 w-full">
+            <input type="text" name='title' className="input input-lg w-full" placeholder="Event title" />
+            <input type="text" name='category' className="input input-lg w-full" placeholder="Category" />            
+            <textarea name='description' className="textarea textarea-lg w-full" placeholder="Description" />
+              <input type="datetime-local" name='date' className="input input-lg w-full" placeholder="Date" />
+              <input type="text" name='city' className="input input-lg w-full" placeholder="City" />
+              <input type="text" name='venue' className="input input-lg w-full" placeholder="Venue" />
               <div className="flex justify-end w-full gap-3">
                 <a onClick={() => setFormOpen(false)} type="button" className="btn btn-neutral">Cancel</a>
-                <a type="submit" className="btn btn-primary">Submit</a> 
+                <button type="submit" className="btn btn-primary">Submit</button> 
               </div>
         </form>
     </div>
