d531c1f7a8bebf537178aa980897f454a0b65d4a
Dan Raymond <kip.raymond@gmail.com>
2025-06-16 16:18:23 -0600

    Finished Section 41.

--- src/features/events/eventSlice.ts
+++ src/features/events/eventSlice.ts
@@ -1,15 +1,35 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { AppEvent } from "../../lib/types";
+import type { AppDispatch, RootState } from "../../lib/stores/store";
 
 type State = {
   events: AppEvent[];
   selectedEvent: AppEvent | null;
+  formOpen: boolean;
 };
 
 const initialState: State = {
   events: [],
   selectedEvent: null,
+  formOpen: false,
 };  
+
+export function toggleForm(event: AppEvent | null) {
+    return function(dispatch: AppDispatch, getState: () => RootState) {
+        const formOpen = getState().event.formOpen;
+
+        if (formOpen) {
+            dispatch(closeForm());
+            setTimeout(() => {
+                dispatch(selectEvent(event));
+                dispatch(openForm());
+            }, 300); // Delay to allow the form to close before reopening
+        } else {
+            dispatch(selectEvent(event));
+            dispatch(openForm()); // Open the form when an event is selected                        
+        }
+    }   
+}
 
 export const eventSlice = createSlice({
   name: "event",
@@ -33,11 +53,17 @@ export const eventSlice = createSlice({
     selectEvent: (state, action: PayloadAction<AppEvent | null>) => {
       state.selectedEvent = action.payload;
     },
+    openForm: (state) => {
+      state.formOpen = true;
+    },
+    closeForm: (state) => {
+      state.formOpen = false;
+    }
 
   },
 }); 
 
-export const {setEvents, createEvent, updateEvent, deleteEvent, selectEvent} = eventSlice.actions;
+export const {setEvents, createEvent, updateEvent, deleteEvent, selectEvent, openForm, closeForm} = eventSlice.actions;
 
 
 
