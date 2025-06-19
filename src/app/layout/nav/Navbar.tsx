663609d7935b5bc8cc6f78678fdd1edba524ddf6
Dan Raymond <kip.raymond@gmail.com>
2025-06-19 17:48:51 -0600

    Finished Section 6.47

--- src/app/layout/nav/Navbar.tsx
+++ src/app/layout/nav/Navbar.tsx
@@ -1,19 +1,16 @@
-import { toggleForm } from "../../../features/events/eventSlice";
-import { useAppDispatch } from "../../../lib/stores/store";
+import { NavLink } from "react-router";
 
 export default function Navbar() {
-    const dispatch = useAppDispatch();
-
     return (
         <header className="p-3 w-full fixed top-0 z-50 bg-gradient-to-r from-primary to-black">
             <div className="flex align-middle items-center px-10 mx-auto gap-6 cursor-pointer">
-                <a className="max-h-16 text-white flex items-center gap-3 border-r-white border-r-2 pr-6">
-                    <h3 className="text-2xl font-semibold text-white uppercase">Re-vents</h3>
-                </a>
+                <div className="max-h-16 text-white flex items-center gap-3 border-r-white border-r-2 pr-6">
+                    <NavLink to="/" className="text-2xl font-semibold text-white uppercase">Re-vents</NavLink>
+                </div>
 
                 <nav className="flex gap-3 my-2 uppercase text-lg text-white">
-                    <a title='Events'>Events</a>
-                    <a onClick={() => dispatch(toggleForm(null))} title="Create">Create</a>
+                    <NavLink to='events' title='Events'>Events</NavLink>
+                    <NavLink to='createEvent' title="Create Event">Create</NavLink>
                 </nav>
 
                 <div className="flex align-middle ml-auto gap-3">
